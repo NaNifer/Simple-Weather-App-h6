67d7c5b50c42433a35bfceb4c5a12504235fbde2
Nifer Kilakila <nifercritter@gmail.com>
2022-06-01 22:41:38 -0700

    html structure, bootstrap, fetch API

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,28 @@
 // the temperature, the wind speed, and the humidity
 // UV index with a color that indicates whether the conditions are favorable, moderate, or severe
 
+
+
+function getApi() {
+    const APIkey = "1954d330fe9f2fbee8fe2fbda687fcb7"
+    let city = "oakland"
+    var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}`;
+  
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
+        })
+  }
+  
+  getApi();
+  
+  
+  
+  
+  // weather icon
+  // var iconcode = a.weather[0].icon;
+  // var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+  
