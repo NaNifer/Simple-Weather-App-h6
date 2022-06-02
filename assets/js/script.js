eb3b85e48c6ed35d8ed86b833f3707a646d6693e
Nifer Kilakila <nifercritter@gmail.com>
2022-06-01 20:47:21 -0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// CRITERIA
+//  5-day forecast that displays the date, 
+// an icon representation of weather conditions, 
+// the temperature, the wind speed, and the humidity
+// UV index with a color that indicates whether the conditions are favorable, moderate, or severe
+
